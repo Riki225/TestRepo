eff7bcc7407d16c1ab380e142e30482290898ab1
Riki225 <79812982+Riki225@users.noreply.github.com>
2021-03-23 15:13:05 +1100

    Random Number Generator Example

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace randomnumbergeneratorexample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            int randomNumber = rand.Next(201);
+
+            if (randomNumber >50) {
+                System.Console.WriteLine($"{randomNumber} isi greater than 50");   
+            } else if (randomNumber ==50) {
+                System.Console.WriteLine($"{randomNumber} isi equal than 50");
+            } else {
+                System.Console.WriteLine($"{randomNumber} isi less than 50");
+            }
+        }
+    }
+}
